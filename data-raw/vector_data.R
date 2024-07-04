6401f68b0f0925789ea0d6931fe913f0e1f08037
Darren Norris <doon75@hotmail.com>
2024-07-04 20:28:51 -0300

    Country summary table.

--- data-raw/vector_data.R
+++ data-raw/vector_data.R
@@ -1,5 +1,5 @@
 ## code to prepare vector data goes here
-library(tidyverse)
+library(dplyr)
 library(usethis)
 
 # .rds with river points made by TACAR https://github.com/darrennorris/TACAR
@@ -12,7 +12,7 @@ points_bau <- inpoints |>
   filter(model_name == "modelkey_BAU") |>
   mutate(flag_50_42y = factor(if_else(fem_diff_t42 <= -0.5, 1, 0))) |>
   select(BASIN_N, subbasn, SUBBASI,
-         BAS_NAME, RIV_ORD, BB_ID, BB_NAME, REACH_ID,
+         BAS_NAME, COUNTRY, RIV_ORD, BB_ID, BB_NAME, REACH_ID,
          Protected, Accessible, Free.flowing,
          fem_diff_t42, flag_50_42y,
          geometry)
